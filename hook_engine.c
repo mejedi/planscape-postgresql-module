181aeaa38f94fd3ad0f7d6406ac115c6cc9aac96
Nick Zavaritsky <mejedi@gmail.com>
2018-04-16 16:27:25 +0200

    Simplify the hook engine

--- hook_engine.c
+++ hook_engine.c
@@ -11,29 +11,11 @@
 #include <string.h>
 #include <assert.h>
 
-// x86_64 registers
-#define RAX  0x0001
-#define RCX  0x0002
-#define RDX  0x0004
-#define RBX  0x0008
-#define RSP  0x0010
-#define RBP  0x0020
-#define RSI  0x0040
-#define RDI  0x0080
-#define R8   0x0100
-#define R9   0x0200
-#define R10  0x0400
-#define R11  0x0800
-#define R12  0x1000
-#define R13  0x2000
-#define R14  0x4000
-#define R15  0x8000
-
 struct Overlay
 {
     uintptr_t target; // Where in address space this will ultimately end up.
     uint8_t  *p;      // Current output position.
-    uint8_t   code[TRAMPOLINE_LEN];
+    uint8_t   code[HOOK_TRAMPOLINE_LEN];
 };
 
 static int g_mem_fd = -1;
@@ -59,94 +41,45 @@ size_t overlay_size(const struct Overlay *c)
 }
 
 static
-int write_jmp(struct Overlay *c, uintptr_t rip_dest, int spare_regs)
-{
-    uintptr_t rip = c->target + overlay_size(c);
-    uintptr_t rip_delta = rip_dest - rip;
-
-    // 8 bit relative target may work
-    if (rip_delta - 2 <= UINT8_MAX || rip_delta - 2 >= INT8_MIN) {
-        c->p[0] = 0xEB;
-        c->p[1] = (uint8_t)(rip_delta - 2);
-        c->p += 2;
-        return 0;
-    }
-
-    // 32 bit relative target may work
-    if (rip_delta - 5 <= UINT32_MAX || rip_delta - 5 >= INT32_MIN) {
-        c->p[0] = 0xE9;
-        put_uint32(&c->p[1], (uint32_t)(rip_delta - 5));
-        c->p += 5;
-        return 0;
-    }
-
-    // movabs @rip_dest, %reg; jmp %reg;
-    if ((spare_regs & 0xffff) == 0)
-        return -1;
-
-    int reg = __builtin_ctz(spare_regs);
-    int reg_hi = reg >> 3;
-    int reg_lo = reg & 7;
-
-    c->p[0] = 0x48 + reg_hi;
-    c->p[1] = 0xB8 + reg_lo;
-    put_uint64(&c->p[2], rip_dest);
-    c->p += 10;
-
-    if (reg_hi != 0) {
-        *(c->p++) = 0x40 + reg_hi;
-    }
-
-    c->p[0] = 0xff;
-    c->p[1] = 0xe0 + reg_lo;
-    c->p += 2;
-
-    return 0;
-}
-
-static
 int install_overlay(int mem_fd, const struct Overlay *c)
 {
     ssize_t c_size = overlay_size(c);
     return pwrite(mem_fd, c->code, c_size, c->target) == c_size ? 0 : -1;
 }
 
-// Oversimplistic register usage detector
 static
-int regs_used(const hde64s *s)
+void write_initial_jmp(struct Overlay *c, uintptr_t target)
 {
-    int regs = 0;
+    // movq target, %rax
+    c->p[0] = 0x48;
+    c->p[1] = 0xB8;
+    put_uint64(c->p + 2, target);
 
-    // Better be safe than sorry
-    if (s->flags & F_MODRM) {
-        regs |= (1 << (s->modrm_reg | (s->rex_r << 3)));
-        regs |= (1 << (s->modrm_rm | (s->rex_b << 3)));
-    }
+    // jmp %rax
+    c->p[10] = 0xFF;
+    c->p[11] = 0xE0;
 
-    // https://github.com/MahdiSafsafi/opcodesDB/blob/master/x86.pl
-    // instructions with reg being part of the opcode
-    int opcode_base;
-    switch (s->opcode) {
-    case 0xC8 ... 0xCF: // bswap
-        opcode_base = 0xC8;
-do_opcode_reg:
-        regs |= (1 << ((s->opcode - opcode_base) | (s->rex_b << 3)));
-        break;
-    case 0xB8 ... 0xBF: // mov
-        opcode_base = 0xB8;
-        goto do_opcode_reg;
-    case 0x58 ... 0x5F: // pop
-        opcode_base = 0x58;
-        goto do_opcode_reg;
-    case 0x50 ... 0x57: // push
-        opcode_base = 0x50;
-        goto do_opcode_reg;
-    case 0x90 ... 0x97: // xchg
-        opcode_base = 0x90;
-        goto do_opcode_reg;
-    }
+    c->p += 12;
+}
 
-    return regs;
+static
+void write_jmp_or_call(uint8_t instr, struct Overlay *c, uintptr_t target, uintptr_t **jump_table)
+{
+    // jmp  *jump_table(%rip)
+    // call *jump_table(%rip)
+    c->p[0] = instr;
+    c->p[1] = 0x25;
+    put_uint32(c->p + 2, (uintptr_t)(*jump_table) - (c->target + overlay_size(c) + 6));
+
+    c->p += 6;
+    (*jump_table)[0] = target;
+    (*jump_table)++;
+}
+
+static inline
+void write_jmp(struct Overlay *c, uintptr_t target, uintptr_t **jump_table)
+{
+    write_jmp_or_call(0xFF, c, target, jump_table);
 }
 
 // Patch function @fn, so that every time it is called, control is
@@ -163,21 +96,21 @@ int hook_install(void *fn, void *replacement, void *trampoline)
     struct Overlay fn_overlay = {(uintptr_t)fn, fn_overlay.code};
     struct Overlay t_overlay  = {(uintptr_t)trampoline, t_overlay.code};
 
-    // These are volatile registers we can safely wreck.
-    // Assume AMD ABI, up to 4 arguments.
-    int spare_regs = ~(RDI | RSI | RDX | RCX | RBX | RBP | RSP | R12 | R13 | R14 | R15);
+    // Jump table is normally extracted from the trampoline. Provide a
+    // placeholder if trampoline is NULL
+    uint64_t jump_table_data[HOOK_JUMP_MAX], *jump_table = jump_table_data;
+
+    // trampoline points to a symbol created with HOOK_DEFINE_TRAMPOLINE
+    // macro; after TRAMPOLINE_LEN area, there's a code sequence
+    // returning a pointer to a per-trampoline jump table
+    if (trampoline)
+        jump_table = ((uint64_t * (*)(void))((uintptr_t)trampoline + HOOK_TRAMPOLINE_LEN))();
 
     // Prepare code to overwrite @fn with. This will be JMP @replacement.
-    rc = write_jmp(&fn_overlay, (uintptr_t)replacement, spare_regs);
-    if (rc != 0) return rc;
-
-    // Deferred jump sequences (Jcc)
-    int       jump_n = 0;
-    uint8_t   jump_pos[JCC_MAX];
-    uintptr_t jump_dest[JCC_MAX];
+    write_initial_jmp(&fn_overlay, (uintptr_t)replacement);
 
     // Detect jumps into the code range we overwrite.
-    uintptr_t rip_hazard = UINTPTR_MAX;
+    const uintptr_t rip_hazard = (uintptr_t)fn + overlay_size(&fn_overlay);
 
     // @fn is going to be partially clobbered. Disassemble and evacuate
     // some instructions.
@@ -191,10 +124,6 @@ int hook_install(void *fn, void *replacement, void *trampoline)
         if (s.flags & F_ERROR) return -1;
         disas_offset += s.len;
 
-        spare_regs &= ~regs_used(&s);
-
-        // TODO: RIP-relative addressing
-
         uintptr_t rip_dest = UINTPTR_MAX;
 
         switch (s.opcode) {
@@ -207,30 +136,31 @@ int hook_install(void *fn, void *replacement, void *trampoline)
             // relative jump, 8 bit immediate offset
             assert(s.flags & F_IMM8);
             rip_dest = (uintptr_t)fn + disas_offset + (int8_t)s.imm.imm8;
-            rc = write_jmp(&t_overlay, rip_dest, spare_regs);
-            if (rc != 0) return rc;
-            s.len = 0;
-            break;
+            write_jmp(&t_overlay, rip_dest, &jump_table);
+            goto check_rip_dest;
 
         case 0xEB:
             // relative jump, 32 bit immediate offset
             assert(s.flags & F_IMM32);
             rip_dest = (uintptr_t)fn + disas_offset + (int32_t)s.imm.imm32;
-            rc = write_jmp(&t_overlay, rip_dest, spare_regs);
-            if (rc != 0) return rc;
-            s.len = 0;
-            break;
+            write_jmp(&t_overlay, rip_dest, &jump_table);
+            goto check_rip_dest;
 
         case 0xE3:
+            // jump if %ecx/ %rcx zero
+            return -1;
+
         case 0x70 ... 0x7f:
             // Jcc jump, 8 bit immediate offset
             assert(s.flags & F_IMM8);
             rip_dest = (uintptr_t)fn + disas_offset + (int8_t)s.imm.imm8;
-            jump_pos[jump_n] = overlay_size(&t_overlay) + s.len;
-            jump_dest[jump_n] = rip_dest;
-            jump_n += 1;
-            // Let the instruction come through as is, will patch later.
-            break;
+
+            t_overlay.p[0] = s.opcode ^ 1;
+            t_overlay.p[1] = 6;
+            t_overlay.p += 2;
+
+            write_jmp(&t_overlay, rip_dest, &jump_table);
+            goto check_rip_dest;
 
         case 0x0F:
             if (s.opcode2 >= 0x80 && s.opcode2 <= 0x8F) {
@@ -239,31 +169,44 @@ int hook_install(void *fn, void *replacement, void *trampoline)
                 rip_dest = (uintptr_t)fn + disas_offset + (int32_t)s.imm.imm32;
 
                 // Convert to a shorter form
-                t_overlay.p[0] = s.opcode2 - 0x10;
+                t_overlay.p[0] = (s.opcode2 - 0x10) ^ 1;
+                t_overlay.p[1] = 6;
                 t_overlay.p += 2;
-                s.len = 0;
 
-                jump_pos[jump_n] = overlay_size(&t_overlay);
-                jump_dest[jump_n] = rip_dest;
-                jump_n += 1;
+                write_jmp(&t_overlay, rip_dest, &jump_table);
+                goto check_rip_dest;
             }
             break;
         }
 
-        // Copy instruction, unless suppressed.
-        if (s.len != 0) {
-            memcpy(t_overlay.p, i, s.len);
-            t_overlay.p += s.len;
+        // RIP-relative addressing
+        if ((s.flags & F_MODRM) &&
+            s.modrm_mod == 0 && s.modrm_rm == 0x5) {
+
+            // LEA?
+            if (s.opcode != 0x8D)
+                return -1;
+
+            // Convert to MOV
+            t_overlay.p[0] = 0x48 + s.rex_r;
+            t_overlay.p[1] = 0xB8 + s.modrm_reg;
+            put_uint64(t_overlay.p + 2, (uintptr_t)fn + disas_offset + (int32_t)s.disp.disp32);
+            t_overlay.p += 10;
+
+            continue;
         }
 
-        // Update rip_hazard
-        if (rip_dest >= (uintptr_t)fn && rip_dest < rip_hazard)
-            rip_hazard = rip_dest;
-    }
+        // Copy instruction
+        memcpy(t_overlay.p, i, s.len);
+        t_overlay.p += s.len;
+        continue;
 
-    // If we've seen a jump into the range we are about to overwrite,
-    // this isn't going to work.
-    if (rip_hazard - (uintptr_t)fn < disas_offset) return -1;
+check_rip_dest:
+        // If we've seen a jump into the range we are about to overwrite,
+        // this isn't going to work.
+        if (rip_dest >= (uintptr_t)fn && rip_dest < rip_hazard)
+            return -1;
+    }
 
     // If we've clobbered a *part* of an instruction, we should beter
     // int3 the surviving part.
@@ -272,15 +215,7 @@ int hook_install(void *fn, void *replacement, void *trampoline)
     fn_overlay.p += partially_clobbered;
 
     // Connect trampoline to the unclobbered part of @fn.
-    rc = write_jmp(&t_overlay, (uintptr_t)fn + disas_offset, spare_regs);
-    if (rc != 0) return rc;
-
-    // Finally fix Jcc-s.
-    for (int i = 0; i < jump_n; i++) {
-        t_overlay.code[jump_pos[i] - 1] = (uint8_t)(overlay_size(&t_overlay) - jump_pos[i]);
-        rc = write_jmp(&t_overlay, jump_dest[i], spare_regs);
-        if (rc != 0) return rc;
-    }
+    write_jmp(&t_overlay, (uintptr_t)fn + disas_offset, &jump_table);
 
     // Now actually owerwrite things.
     int mem_fd = g_mem_fd;
--- hook_engine.h
+++ hook_engine.h
@@ -6,22 +6,25 @@ extern "C" {
 
 #ifdef __x86_64__
 
-// The longest instruction sequence we may emit to implement
-// unconditional jump.           (MOV %reg, imm64; JMP %reg;)
-#define JUMP_SEQUENCE_LEN     13
+// The length of a jump sequence a hooked function's code is clobbered
+// with.
+#define HOOK_INITIAL_JUMP_LEN 12
 
-// The length of instruction sequence destroyed while patching absolute
-// jump in.                      (JUMP_SEQUENCE_LEN + 1 following
-//                                partially destroyed instruction)
-#define CLOBBERED_CODE_LEN    (JUMP_SEQUENCE_LEN + 14)
+// The length of a jump sequence in trampoline's body. This is different
+// from HOOK_INITIAL_JUMP_LEN since we can't clobber registers.
+#define HOOK_JUMP_LEN         6
 
-// Maximum number of Jcc-s opcodes we may have to evacuate.
-// Jcc-s are special, since we have to emit a jump sequence for each one
-// of them.
-#define JCC_MAX               (JUMP_SEQUENCE_LEN/2 + 1)
+// Max length of the code clobbered in a hooked functions (account for a
+// partially clobbered following instructions, max instruction length in
+// x86_64 is 15).
+#define HOOK_CLOBBERED_LEN    (HOOK_INITIAL_JUMP_LEN + 14)
+
+// Maximum number of jumps/calls in a trampoline body (shortest
+// jump/call instruction in x86_64 is 2 bytes long).
+#define HOOK_JUMP_MAX         (HOOK_INITIAL_JUMP_LEN/2 + 1)
 
 // Maximum size of trampoline code.
-#define TRAMPOLINE_LEN        (CLOBBERED_CODE_LEN + JCC_MAX * JUMP_SEQUENCE_LEN)
+#define HOOK_TRAMPOLINE_LEN   (HOOK_CLOBBERED_LEN + HOOK_JUMP_MAX * HOOK_JUMP_LEN)
 
 // Define trampoline function. Merely reserves bytes in .code section.
 // These bytes are to be later patched by hook_install().
@@ -30,7 +33,13 @@ extern "C" {
 // int my_trampoline(int param);
 // HOOK_DEFINE_TRAMPOLINE(my_trampoline);
 #define HOOK_DEFINE_TRAMPOLINE(name) \
-__asm__(".globl " HOOK_S(name) "\n" HOOK_S(name) ":\n\t.skip " HOOK_S(TRAMPOLINE_LEN) ", 0xcc\n")
+__asm__(".globl " HOOK_S(name) "\n" \
+        HOOK_S(name) ":\n" \
+        "\t.skip " HOOK_S(HOOK_TRAMPOLINE_LEN) ", 0xcc\n" \
+        "\tleaq _J_" HOOK_S(name) "(%rip), %rax;\n" \
+        "\tret\n" \
+        ".local _J_" HOOK_S(name) "\n" \
+        ".comm _J_" HOOK_S(name) ", " HOOK_S(HOOK_JUMP_MAX*8) ", 8\n")
 
 #else
 #error Unsupported ARCH :(
