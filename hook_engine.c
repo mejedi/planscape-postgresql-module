db748ebb348e5d3dee21a414a916dccc66eab7fa
Nick Zavaritsky <mejedi@gmail.com>
2018-07-05 15:50:31 +0200

    Update hook engine
     * Fallback in case /proc/self/mem is not available, using mprotect().
    
     * Helpful error messages, sufficient context is included so
       that it is possible to troubleshoot issues in the field.  In case the
       failure was due to disassembler error or there were some instructions
       encountered we were unable to handle, a hexdump of the function's code
       is included.
    
     * Robust trampoline validation. It is no longer possible to pass
       arbitrary function as a trampoline.
    
     * Breaking change: once hook_install() writes over a trampoline, the
       trampoline is 'sealed' and any subsequent attempt to reuse it in
       hook_install() will fail.
    
     * Improvements in C++ wrapper - simplify code, provide default value
       for trampoline parameter (nullptr).

--- hook_engine.c
+++ hook_engine.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE 1
+
 #ifndef __x86_64__
 #error Unsupported ARCH :(
 #endif
@@ -6,10 +8,14 @@
 #include "hde/hde64.h"
 
 #include <sys/types.h>
+#include <sys/mman.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <errno.h>
+#include <stdio.h>
 #include <assert.h>
+#include <dlfcn.h>
 
 struct Overlay
 {
@@ -18,7 +24,38 @@ struct Overlay
     uint8_t   code[HOOK_TRAMPOLINE_LEN];
 };
 
-static int g_mem_fd = -1;
+static __thread int g_mem_fd = -1;
+static __thread char g_errmsg[256];
+
+#define FORMAT_ERRMSG(...) snprintf(g_errmsg, sizeof g_errmsg, __VA_ARGS__)
+
+// Dump as many bytes as fit into buf, but don't cross page boundary.
+static
+const char *hexdump(const void *mem, char *buf, size_t size)
+{
+    uintptr_t page_mask    = sysconf(_SC_PAGESIZE) - 1;
+    const uint8_t *mem_p   = mem;
+    const uint8_t *mem_end = (const uint8_t *)(((uintptr_t)mem + page_mask) & ~page_mask);
+
+    char *buf_p            = buf;
+    char *buf_end          = buf + size;
+
+    while (mem_p < mem_end && buf_p + 3 < buf_end)
+        buf_p += sprintf(buf_p, "%02x", *mem_p++);
+
+    return buf;
+}
+
+static
+const char *funcname(void *func)
+{
+    Dl_info info;
+
+    if (dladdr(func, &info) == 0 || !info.dli_sname)
+        return "Unknown";
+
+    return info.dli_sname;
+}
 
 static inline
 void put_uint32(uint8_t *p, uint32_t v)
@@ -41,10 +78,33 @@ size_t overlay_size(const struct Overlay *c)
 }
 
 static
-int install_overlay(int mem_fd, const struct Overlay *c)
+int install_overlay(const struct Overlay *c)
 {
     ssize_t c_size = overlay_size(c);
-    return pwrite(mem_fd, c->code, c_size, c->target) == c_size ? 0 : -1;
+
+    if (g_mem_fd == -1) {
+        // No /proc/self/mem available, use mprotect.
+
+        uintptr_t page_mask  = sysconf(_SC_PAGESIZE) - 1;
+        uintptr_t page_begin = c->target & ~page_mask;
+        uintptr_t page_end   = (c->target + c_size + page_mask) & ~page_mask;
+
+        if (mprotect((void*)page_begin, page_end - page_begin, PROT_WRITE|PROT_EXEC) != 0)
+            goto error;
+
+        memcpy((void*)c->target, c->code, c_size);
+
+        mprotect((void*)page_begin, page_end - page_begin, PROT_EXEC);
+        return 0;
+    }
+
+    if (pwrite(g_mem_fd, c->code, c_size, c->target) == c_size) {
+        return 0;
+    }
+
+error:
+    FORMAT_ERRMSG("patching code in memory: %s", strerror(errno));
+    return -1;
 }
 
 static
@@ -83,6 +143,28 @@ void write_call(struct Overlay *c, uintptr_t target, uintptr_t **jump_table)
     c->p[-5] = 0x15;
 }
 
+// used by get_jump_table() for validation
+void reference_trampoline(void);
+HOOK_DEFINE_TRAMPOLINE(reference_trampoline);
+
+static
+uint64_t *get_jump_table(void *trampoline)
+{
+    // validate trampoline - must be in a clean state,
+    // HOOK_TRAMPOLINE_LEN * (int 3) instructions,
+    // followed by lea [rip-relative], %rax.
+    // +3 in memcmp below to check lea instruction (minus offset part).
+    if (memcmp(trampoline, (void *)reference_trampoline,
+               HOOK_TRAMPOLINE_LEN + 3) != 0) {
+        return NULL;
+    }
+
+    // trampoline points to a symbol created with HOOK_DEFINE_TRAMPOLINE
+    // macro; after TRAMPOLINE_LEN area, there's a code sequence
+    // returning a pointer to a per-trampoline jump table
+    return ((uint64_t*(*)(void)) ((uintptr_t)trampoline + HOOK_TRAMPOLINE_LEN)) ();
+}
+
 // Patch function @fn, so that every time it is called, control is
 // transferred to @replacement.
 //
@@ -90,8 +172,6 @@ void write_call(struct Overlay *c, uintptr_t target, uintptr_t **jump_table)
 // transferred to @trampoline.
 int hook_install(void *fn, void *replacement, void *trampoline)
 {
-    int rc = 0;
-
     // We render code in two overlays, and later overwrite @fn and
     // @trampoline with the overlays' content.
     struct Overlay fn_overlay = {(uintptr_t)fn, fn_overlay.code};
@@ -101,11 +181,10 @@ int hook_install(void *fn, void *replacement, void *trampoline)
     // placeholder if trampoline is NULL
     uint64_t jump_table_data[HOOK_JUMP_MAX], *jump_table = jump_table_data;
 
-    // trampoline points to a symbol created with HOOK_DEFINE_TRAMPOLINE
-    // macro; after TRAMPOLINE_LEN area, there's a code sequence
-    // returning a pointer to a per-trampoline jump table
-    if (trampoline)
-        jump_table = ((uint64_t*(*)(void)) ((uintptr_t)trampoline + HOOK_TRAMPOLINE_LEN)) ();
+    if (trampoline && !(jump_table = get_jump_table(trampoline))) {
+        FORMAT_ERRMSG("bad trampoline pointer");
+        return -1;
+    }
 
     // Prepare code to overwrite @fn with. This will be JMP @replacement.
     write_initial_jmp(&fn_overlay, (uintptr_t)replacement);
@@ -117,18 +196,28 @@ int hook_install(void *fn, void *replacement, void *trampoline)
 
     while (rip < rip_hazzard) {
 
-        hde64s s;
+        hde64s    s;
         uintptr_t rip_dest = UINTPTR_MAX;
+        char      hexdump_buf[HOOK_CLOBBERED_LEN*2 + 1];
 
         hde64_disasm((const uint8_t *)rip, &s);
-        if (s.flags & F_ERROR) return -1;
+        if (s.flags & F_ERROR) {
+            FORMAT_ERRMSG("creating trampoline for %s: unknown instruction at offset %+d (%s)",
+                          funcname(fn),
+                          (int)(rip - (uintptr_t)fn),
+                          hexdump(fn, hexdump_buf, sizeof hexdump_buf));
+            return -1;
+        }
 
         rip += s.len;
 
         switch (s.opcode) {
 
         case 0xCC:
-            // int3: this is probably a breakpoint set by a debugger
+            FORMAT_ERRMSG("creating trampoline for %s: 'INT 3' instruction, "
+                          "looks like a breakpoint set by debugger (%s)",
+                          funcname(fn),
+                          hexdump(fn, hexdump_buf, sizeof hexdump_buf));
             return -1;
 
         case 0xE8:
@@ -154,6 +243,9 @@ int hook_install(void *fn, void *replacement, void *trampoline)
 
         case 0xE3:
             // jump if %ecx/ %rcx zero
+            FORMAT_ERRMSG("creating trampoline for %s: 'JCXZ' instruction not supported (%s)",
+                          funcname(fn),
+                          hexdump(fn, hexdump_buf, sizeof hexdump_buf));
             return -1;
 
         case 0x70 ... 0x7f:
@@ -190,8 +282,12 @@ int hook_install(void *fn, void *replacement, void *trampoline)
             s.modrm_mod == 0 && s.modrm_rm == 0x5) {
 
             // LEA?
-            if (s.opcode != 0x8D)
+            if (s.opcode != 0x8D) {
+                FORMAT_ERRMSG("creating trampoline for %s: %%rip-relative addressing (%s)",
+                              funcname(fn),
+                              hexdump(fn, hexdump_buf, sizeof hexdump_buf));
                 return -1;
+            }
 
             // Convert to MOV
             t_overlay.p[0] = 0x48 + s.rex_r;
@@ -210,8 +306,13 @@ int hook_install(void *fn, void *replacement, void *trampoline)
 check_rip_dest:
         // If we've seen a jump into the range we are about to overwrite,
         // this isn't going to work.
-        if (rip_dest >= (uintptr_t)fn && rip_dest < rip_hazzard)
+        if (rip_dest > (uintptr_t)fn && rip_dest < rip_hazzard) {
+            FORMAT_ERRMSG("creating trampoline for %s: a jump into the "
+                          "clobbered instruction range encountered (%s)",
+                          funcname(fn),
+                          hexdump(fn, hexdump_buf, sizeof hexdump_buf));
             return -1;
+        }
     }
 
     // If we've clobbered a *part* of an instruction, we should beter
@@ -224,32 +325,19 @@ check_rip_dest:
     write_jmp(&t_overlay, rip, &jump_table);
 
     // Now actually owerwrite things.
-    int mem_fd = g_mem_fd;
-
-    if (mem_fd == -1)
-        mem_fd = open("/proc/self/mem", O_RDWR);
-
-    if (mem_fd == -1)
+    if (trampoline && install_overlay(&t_overlay) != 0)
         return -1;
 
-    if (trampoline)
-        rc = install_overlay(mem_fd, &t_overlay);
-
-    if (rc == 0)
-        rc = install_overlay(mem_fd, &fn_overlay);
-
-    if (mem_fd != g_mem_fd)
-        close(mem_fd);
-
-    return rc;
+    return install_overlay(&fn_overlay);
 }
 
 int hook_begin()
 {
-    if (g_mem_fd == -1)
-        g_mem_fd = open("/proc/self/mem", O_RDWR);
+    if (g_mem_fd == -1) {
+        g_mem_fd = open("/proc/self/mem", O_WRONLY);
+    }
 
-    return g_mem_fd == -1 ? -1 : 0;
+    return 0;
 }
 
 void hook_end()
@@ -258,4 +346,9 @@ void hook_end()
         close(g_mem_fd);
 
     g_mem_fd = - 1;
+}
+
+const char *hook_last_error()
+{
+    return g_errmsg;
 }
--- hook_engine.h
+++ hook_engine.h
@@ -15,7 +15,7 @@ extern "C" {
 #define HOOK_JUMP_LEN         6
 
 // Max length of the code clobbered in a hooked functions (account for a
-// partially clobbered following instructions, max instruction length in
+// partially clobbered following instruction, max instruction length in
 // x86_64 is 15).
 #define HOOK_CLOBBERED_LEN    (HOOK_INITIAL_JUMP_LEN + 14)
 
@@ -45,19 +45,52 @@ __asm__(".globl " HOOK_S(name) "\n" \
 #error Unsupported ARCH :(
 #endif
 
+// hook_install(fn, replacement, trampoline [optional])
+//
 // Patch function @fn, so that every time it is called, control is
 // transferred to @replacement.
 //
 // If @trampoline is provided, instructions destroyed in @fn are
 // transferred to @trampoline.
+//
+// Returns: 0 if succeeded, non-zero on error, check hook_last_error()
 int hook_install(void *fn, void *replacement, void *trampoline);
 
 // Install multiple hooks faster by enclosing calls to hook_install()
 // into hook_begin()/hook_end().
+//
+// Returns: 0 if succeeded, non-zero on error, check hook_last_error()
 int hook_begin(void);
 
 // See hook_begin().
 void hook_end(void);
+
+// hook_last_error(): Last error description string.
+//
+// Imagine you get a message:
+//
+// creating trampoline for outNode: 'INT 3'
+// instruction, looks like a breakpoint set by debugger
+// (cc85f60f84070100008b068d9027ffffff83fa02761a2dd400)
+//
+// In parens you can see a hexdump. In order to decode it:
+//
+// $ echo -n cc85f60f84070100008b068d9027ffffff83fa02761a2dd400 | xxd -r -p | ndisasm -b 64 -
+//
+// Which yields:
+//
+// 00000000  CC                int3
+// 00000001  85F6              test esi,esi
+// 00000003  0F8407010000      jz near 0x110
+// 00000009  8B06              mov eax,[rsi]
+// 0000000B  8D9027FFFFFF      lea edx,[rax-0xd9]
+// 00000011  83FA02            cmp edx,byte +0x2
+// 00000014  761A              jna 0x30
+// 00000016  2D                db 0x2d
+// 00000017  D4                db 0xd4
+// 00000018  00                db 0x00
+//
+const char *hook_last_error(void);
 
 #define HOOK_S(s) HOOK__S(s)
 #define HOOK__S(s) #s
@@ -67,10 +100,14 @@ void hook_end(void);
 
 #include <type_traits>
 
-// Validate functions compatibility. 
-template<typename Fn, typename = typename std::enable_if<std::is_function<
-                                 typename std::remove_pointer<Fn>::type>::value>::type>
-inline int hook_install(Fn &fn, Fn &replacement, Fn &trampoline)
+// hook_install(fn, replacement, trampoline [optional])
+//
+// Patch function @fn, so that every time it is called, control is
+// transferred to @replacement.
+//
+// Ensures functions compatibility.
+template<typename Fn, typename = typename std::enable_if<std::is_function<Fn>::value>::type>
+inline int hook_install(Fn *fn, Fn *replacement, Fn *trampoline = nullptr)
 {
     return hook_install((void *)fn,
                         (void *)replacement,
